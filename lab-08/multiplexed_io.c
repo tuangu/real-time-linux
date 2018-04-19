24673f5a9e6a06a62b4a5f2d460a2b1503bc88eb
Tuan Nguyen <nguyenanhtuan96dn@gmail.com>
2018-04-19 08:42:26 +0300

    lab 8 exit when all data read

--- lab-08/multiplexed_io.c
+++ lab-08/multiplexed_io.c
@@ -8,7 +8,8 @@
 
 #include "MultiSensorSimulator.h"
 
-#define DESC_NUM 10
+#define DESC_NUM    10
+#define STEPS       2
 
 int maxFd(int *i, int);
 
@@ -20,34 +21,29 @@ int main() {
     struct timeval timeout;
     Tmeas measurement;
     int count = 0;
+    int fdDone = 0;
 
     // fill the timeout struct
     timeout.tv_usec = 0;
     timeout.tv_sec = 20;
 
-    // Start the simulator
-    // if (StartSimulator(sensorDescriptors, 5) != 10) {
-    //    printf("Cannot start simulator\n");
-    //    exit(1);
-    // }
-    int n = StartSimulator(sensorDescriptors, 5);
-    printf("Start %d sensors\n", n);
-    sleep(1);
+    StartSimulator(sensorDescriptors, STEPS);
 
     int maxfd = maxFd(sensorDescriptors, DESC_NUM) + 1;
     printf("Max fd: %d\n", maxfd);
 
     // reading sensor data
-    while (1) {
+    while (fdDone < DESC_NUM) {
         FD_ZERO(&fdset);
         for (int i = 0; i < DESC_NUM; i++) {
             // -1 mean that the descrtiptor has reached the end of file
             if (sensorDescriptors[i] != -1) {
                 FD_SET(sensorDescriptors[i], &fdset);
             }
+
         }
 
-        fdReady = select(maxfd, &fdset, NULL, NULL, &timeout);
+        fdReady = select(maxFd(sensorDescriptors, DESC_NUM) + 1, &fdset, NULL, NULL, &timeout);
         printf("fdReady: %d\n", fdReady);
 
         if (fdReady > 0) {
@@ -58,26 +54,26 @@ int main() {
                     // if the descriptor has reached the EOF, set it to -1
                     if (readRet == 0) {
                         sensorDescriptors[i] = -1;
+                        ++fdDone;
                     } else {
                         ++count;
                         printf("[%d] Sensor %d: %d\n", count, i, measurement.value);
                     }
                 }
             }
-        } else if (fdReady == 0) {
-            printf("Received all sensors' values\n");
-            exit(0);
-        } else {
+        } else if (fdReady < 0) {
             printf("ERROR: an error occured when calling select\n");
             exit(1);
+        } else if (fdReady == 0) {
+            exit(0);
         }
     }
-    
+    printf("All sensors' data received\n");
     exit(0);
 }
 
 int maxFd(int fd[], int n) {
-    int max = 0;
+    int max = fd[0];
     for (int i = 0; i < n; i++) {
         printf("fd%d: %d\n", i, fd[i]);
         if (fd[i] > max)
